4b7185180c0ed94675040c647854db5718d470c9
Alessandro B Lemos Jr <ablemosjunior@gmail.com>
2023-01-03 20:27:05 -0300

    consumo de api profile e att de pagina portfolio

--- portfolio/assets/js/api.js
+++ portfolio/assets/js/api.js
@@ -1,4 +1,7 @@
 
-function fetchProfileData() {
-  const url = '';
+async function fetchProfileData() {
+  const url = 'https://raw.githubusercontent.com/ablemosjr/frontEnd-Estudo/portfolio-api/portfolio/data/profile.json';
+  const fetching = await fetch(url);
+  return await fetching.json();
 }
+
