b322ec9f81efa81a13a6ec20ace9afe2da3c0d97
Alessandro B Lemos Jr <ablemosjunior@gmail.com>
2023-04-04 16:01:37 -0300

    add componente contato e validacao de forms

--- ang-eCommerce/src/app/contato/contato.module.ts
+++ ang-eCommerce/src/app/contato/contato.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+import { NgxMaskPipe, NgxMaskDirective, provideNgxMask } from 'ngx-mask';
 
 import { ContatoRoutingModule } from './contato-routing.module';
 import { ContatoComponent } from './contato.component';
-
 
 @NgModule({
   declarations: [
@@ -11,7 +12,11 @@ import { ContatoComponent } from './contato.component';
   ],
   imports: [
     CommonModule,
-    ContatoRoutingModule
-  ]
+    ContatoRoutingModule,
+    ReactiveFormsModule,
+    NgxMaskDirective,
+    NgxMaskPipe
+  ],
+  providers: [provideNgxMask()]
 })
 export class ContatoModule { }
