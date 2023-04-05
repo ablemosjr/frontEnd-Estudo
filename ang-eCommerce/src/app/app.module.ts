457de3329619a7da607972379130a8478424546b
Alessandro B Lemos Jr <ablemosjunior@gmail.com>
2023-04-05 15:04:07 -0300

    add pesquisa por barra de pesquisa

--- ang-eCommerce/src/app/app.module.ts
+++ ang-eCommerce/src/app/app.module.ts
@@ -3,24 +3,29 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { FormsModule } from '@angular/forms';
 
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { AppRoutingModule } from './app-routing.module';
 import { NaoEncontradaComponent } from './nao-encontrada/nao-encontrada.component';
+import { BarraPesquisaComponent } from './barra-pesquisa/barra-pesquisa.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    NaoEncontradaComponent
+    NaoEncontradaComponent,
+    BarraPesquisaComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatSnackBarModule
+    MatSnackBarModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
